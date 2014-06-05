83b39a2d5d56dc17fc3880ee4dfb594ed6d989b8
mikenewt <mikejnewt@gmail.com>
2014-06-06 07:52:01 +0800

    Guessing/answering partially done.
    Working on the branch instead of master again. :(

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-var questionCount = 0;
+var userAnswer, question, qNum = 0;
+var questionLibrary = [{'questionNumber': 1,
+						'lyrics': 'Good eye sniper! I shoot, you run',
+						'answer': 'afavorhouseatlantic'},
+						{'questionNumber': 2,
+						 'lyrics': 'Sir, I think you\'d better take my hand and pray we\'ll make this one out alive',
+						 'answer': 'junesongprovision'},
+						{'questionNumber': 3,
+						 'lyrics': 'Pain is only a pulse if you just stop feeling it',
+						 'answer': 'timeconsumer'},
+						{'questionNumber': 4,
+						 'lyrics': 'If he\'s not here then where?',
+						 'answer': 'theafterman'},
+						{'questionNumber': 5,
+						 'lyrics': 'I gave my everything for all the wrong things',
+						 'answer': 'darksideofme'}];
 
 $(document).ready(function () {
 
@@ -22,7 +37,12 @@ $(document).ready(function () {
 		$('.overlay-content').slideUp();
 	});
 
-
+	$('#answer-input').keydown(function(event) {
+		if (event.which == 13) {
+			event.preventDefault();
+			answerQuestion();
+		}
+	});
 
 
 
@@ -41,6 +61,7 @@ function startGame() {
 	startButton.css('display', 'none');
 	gameContent.css('display', 'inline-block');
 	input.css('display', 'inline-block');
+	input.focus();
 	questionBubbles.css('display', 'inline-block');
 
 	getQuestion();
@@ -48,11 +69,36 @@ function startGame() {
 
 function getQuestion() {
 
-	var questions = [];
-	var questionContainer = $('#starter-content li')
+	var questionContainer = $('#starter-content')
 
-	for(var i = 0; i < questionContainer.length; i++) {
-		questions.push(questionContainer[i]);
-	}
+	// get a question from the questionDict where the key = questionCount and store in question variable
+	question = questionLibrary[qNum]['lyrics'];
+	console.log(question);
 
+	// dump question text into li .question-content
+	questionContainer.append("<li class='question-content'>" + question + "</li>");
+	qNum = qNum++;
+	
+}
+
+function answerQuestion() {
+	
+	userAnswer = $('#answer-input').val();
+	userAnswer = userAnswer.toLowerCase();
+	userAnswer = userAnswer.replace(/\s/g,'');
+	console.log(userAnswer);
+
+	var answer = questionLibrary[qNum]['answer']
+	console.log(answer);
+
+	if(userAnswer === answer) {
+		$('#progress-tracker li:nth-child(1)').removeClass('unanswered').addClass('correct');
+	} else {
+		$('#progress-tracker li:nth-child(1)').removeClass('unanswered').addClass('incorrect');
+	} 
+
+}
+
+function validateInput() {
+	
 }

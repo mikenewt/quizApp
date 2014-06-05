8cc229b16a4ab017b5d49efdddaf3748de2d2d8a
mikenewt <mikejnewt@gmail.com>
2014-06-05 17:18:02 +0800

    Initial commit.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+var questionCount = 0;
+
 $(document).ready(function () {
 
-	$('a').click(function(e) {
-		e.preventDefault();
+	$('a').click(function(event) {
+		console.log("Handler for anchor text preventDefault() called!");
+		event.preventDefault();
 	});
+
+	$('#start-game').click(function(event) {
+		console.log("Handler for submit preventDefault() called!");
+		event.preventDefault();
+	})
+
+	$('#start-game').click(startGame);
 
 	$('#what').click(function() {
 		$('.overlay-content').slideToggle();
@@ -18,3 +28,31 @@ $(document).ready(function () {
 
 
 })
+
+function startGame() {
+
+	console.log("Running startGame()!");
+	
+	var gameContent = $('.game-content');
+	var input = $('#answer-input');
+	var questionBubbles = $('#bottom-content');
+	var startButton = $('#start-game')
+
+	startButton.css('display', 'none');
+	gameContent.css('display', 'inline-block');
+	input.css('display', 'inline-block');
+	questionBubbles.css('display', 'inline-block');
+
+	getQuestion();
+}
+
+function getQuestion() {
+
+	var questions = [];
+	var questionContainer = $('#starter-content li')
+
+	for(var i = 0; i < questionContainer.length; i++) {
+		questions.push(questionContainer[i]);
+	}
+
+}
